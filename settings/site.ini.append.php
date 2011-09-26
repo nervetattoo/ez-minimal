87e0154530ebcf271f3d844bd1ca8c65d32c5bd9
Raymond Julin <raymond@keyteq.no>
2011-09-26 09:10:06 +0200

    Initial site design / development extension

---
+++ settings/site.ini.append.php
@@ -0,0 +1,21 @@
+<?php /*
+
+[ContentSettings] 
+ViewCaching=disabled
+ 
+[DebugSettings]
+Debug=inline
+DebugOutput=enabled
+DebugRedirection=disabled
+ 
+[OverrideSettings] 
+Cache=disabled
+ 
+[TemplateSettings]
+ShowXHTMLCode=disabled
+ShowUsedTemplates=enabled
+TemplateCompile=disabled
+TemplateCache=disabled
+DevelopmentMode=enabled
+
+*/ ?>
